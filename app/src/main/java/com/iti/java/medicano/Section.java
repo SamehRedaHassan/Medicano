91da6bffcf2efc5a69d827e6228c38f10f602a22
SamehRedaHassan <97190826+SamehRedaHassan@users.noreply.github.com>
2022-04-15 13:01:19 +0200

    add dependent and medication fragment ui

--- app/src/main/java/com/iti/java/medicano/Section.java
+++ app/src/main/java/com/iti/java/medicano/Section.java
@@ -1,2 +1,29 @@
-package com.iti.java.medicano;public class Section {
+package com.iti.java.medicano;
+
+import java.util.List;
+
+public class Section {
+    private String sectionName;
+    private List<MyData> sectionItems;
+
+    public Section(String sectionName, List<MyData> sectionItems) {
+        this.sectionName = sectionName;
+        this.sectionItems = sectionItems;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public List<MyData> getSectionItems() {
+        return sectionItems;
+    }
+
+    public void setSectionItems(List<MyData> sectionItems) {
+        this.sectionItems = sectionItems;
+    }
 }
