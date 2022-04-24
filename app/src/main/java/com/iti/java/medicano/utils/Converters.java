906a41c95612c8d6652f3747a3f61dad36e1e427
abdelwahabzayed <ammz1997@gmail.com>
2022-04-24 18:19:27 +0200

    upDateDatabase()

--- app/src/main/java/com/iti/java/medicano/utils/Converters.java
+++ app/src/main/java/com/iti/java/medicano/utils/Converters.java
@@ -11,7 +11,6 @@ import com.iti.java.medicano.model.RefillReminder;
 import com.iti.java.medicano.model.Reminder;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -28,51 +27,61 @@ public class Converters {
     public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
     }
-    @TypeConverter
-    public  static  RefillReminder getRefillFromString(String value){
-        return new RefillReminder(Integer.parseInt(value.split(";")[0]),Integer.parseInt(value.split(";")[1]));
-    }
-    @TypeConverter
-    public  static  String RefillReminderToString(RefillReminder reminder){
-        return reminder.currentNumOfPills + ";" + reminder.countToReminderWhenReady;
-    }
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @TypeConverter
-    public  static List<Integer> getdays(String value){
 
-        return  new ArrayList<Integer>(Stream.of(value.split(";")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList())) ;
+    @TypeConverter
+    public static RefillReminder getRefillFromString(String value) {
+        return new Gson().fromJson(value, RefillReminder.class);
+        }
+
+    @TypeConverter
+    public static String RefillReminderToString(RefillReminder reminder) {
+        return new Gson().toJson(reminder);
     }
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @TypeConverter
-    public  static String getDays(List<Integer> days){
+    public static List<Integer> getdays(String value) {
+
+        return new ArrayList<Integer>(Stream.of(value.split(";")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @TypeConverter
+    public static String getDays(List<Integer> days) {
         return days.stream().map(Object::toString).collect(Collectors.joining(";"));
     }
 
     @TypeConverter
-    public static String getStringListOfReminders(List<Reminder> reminders){
+    public static String getStringListOfReminders(List<Reminder> reminders) {
         return new Gson().toJson(reminders);
     }
 
 
     @TypeConverter
-    public static List<Reminder> getListOfRemindersFromString(String reminders){
-        return new Gson().fromJson(reminders,new TypeToken<List<Reminder>>(){}.getType());
+    public static List<Reminder> getListOfRemindersFromString(String reminders) {
+        return new Gson().fromJson(reminders, new TypeToken<List<Reminder>>() {
+        }.getType());
     }
+
     @TypeConverter
-    public static String getStringToList(List<String> list){
+    public static String getStringToList(List<String> list) {
         return new Gson().toJson(list);
     }
 
     @TypeConverter
-    public static List<String> getListString(String str){
-        return new Gson().fromJson(str,new TypeToken<List<String>>(){}.getType());
+    public static List<String> getListString(String str) {
+        return new Gson().fromJson(str, new TypeToken<List<String>>() {
+        }.getType());
     }
+
     @TypeConverter
-    public static HashMap<String,String> getInvitations(String str){
-        return new Gson().fromJson(str,new TypeToken<HashMap<String,String>>(){}.getType());
+    public static HashMap<String, String> getInvitations(String str) {
+        return new Gson().fromJson(str, new TypeToken<HashMap<String, String>>() {
+        }.getType());
     }
+
     @TypeConverter
-    public static String getStringToHashMAp(HashMap<String,String> list){
+    public static String getStringToHashMAp(HashMap<String, String> list) {
         return new Gson().toJson(list);
     }
 }
